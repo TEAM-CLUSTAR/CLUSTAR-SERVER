00ec256ade16f3174726e83cff6e80f83e626a18
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-01-04 21:19:55 +0900

    init: 사소한 수정

--- src/main/java/org/project/global/exception/BusinessException.java
+++ src/main/java/org/project/global/exception/BusinessException.java
@@ -1,13 +1,13 @@
 package org.project.global.exception;
 
 import lombok.Getter;
-import org.project.global.exception.errorcode.GlobalErrorCode;
+import org.project.global.exception.errorcode.ErrorCode;
 
 @Getter
 public class BusinessException extends RuntimeException {  // 예외클래스 상속의 중간층
-    private final GlobalErrorCode errorCode;
+    private final ErrorCode errorCode;
 
-    public BusinessException(GlobalErrorCode errorCode) {
+    public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMsg());
         this.errorCode = errorCode;
     }
