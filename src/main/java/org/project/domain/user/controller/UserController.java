47ded09ae4eaf641ccc25727eeb97538b49577fd
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-01-04 21:04:19 +0900

    Init:Swagger 테스트

--- src/main/java/org/project/domain/user/controller/UserController.java
+++ src/main/java/org/project/domain/user/controller/UserController.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.project.domain.user.service.UserService;
-import org.project.global.api.ApiResponse;
+import org.project.global.annotation.BusinessExceptionDescription;
+import org.project.global.config.swagger.SwaggerResponseDescription;
+import org.project.global.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,7 @@ public class UserController {
 
     @GetMapping(value = "/mypage/user")  // 유저의 이름, 사용가능한 크레딧 개수 조회
     @Operation(summary = "마이페이지 기본 정보 제공 API")
+    @BusinessExceptionDescription(SwaggerResponseDescription.GET_USER)
     public ResponseEntity<ApiResponse<String>> hello() {
 
         return ResponseEntity.ok(ApiResponse.ok("hello"));
