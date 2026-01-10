23ead5de4681b9d2aa315319ff14a01dd0f7f6a7
koobontak <kbt82883@gmail.com>
2026-01-10 16:58:25 +0900

    [feat] UserDetails 구현 클래스 작성

---
+++ src/main/java/org/project/domain/user/dto/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package org.project.domain.user.dto;
+
+import lombok.Getter;
+import org.project.domain.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // 필요에 따라 ROLE 처리 가능
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail(); // 로그인 기준 (예: 이메일)
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+}
