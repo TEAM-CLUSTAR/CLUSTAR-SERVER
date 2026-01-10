a552637dcf1c5ad25350995ee91236c82e296008
koobontak <kbt82883@gmail.com>
2026-01-10 16:53:03 +0900

    [feat] User엔티티 필드 추가

--- src/main/java/org/project/domain/user/entity/User.java
+++ src/main/java/org/project/domain/user/entity/User.java
@@ -2,6 +2,7 @@ package org.project.domain.user.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.project.global.entity.BaseEntity;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -9,8 +10,35 @@ import lombok.*;
 @Getter
 @Builder
 @Table(name = "users")
-public class User {
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "profile_image_url", nullable = false)
+    private String profileImageUrl;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "provider_name", nullable = false)
+    private String providerName;
+
+
+    /**
+     * 소셜 로그인 사용자 생성 (최초 회원가입 시)
+     * - email / name / profileImageUrl은 소셜에서 내려오는 값
+     * - providerName은 OAuth 제공회사 이름
+     */
+    public static User createSocialUser(String email, String name, String profileImageUrl, String providerName) {
+        return User.builder()
+                .email(email)
+                .name(name)
+                .profileImageUrl(profileImageUrl)
+                .providerName(providerName)
+                .build();
+    }
 }
