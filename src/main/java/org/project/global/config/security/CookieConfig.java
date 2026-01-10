96d13a00183e1bf43ccc1643a4916537dbb7afc4
koobontak <kbt82883@gmail.com>
2026-01-10 16:54:16 +0900

    [chore] 쿠키관련 환경변수 추가

---
+++ src/main/java/org/project/global/config/security/CookieConfig.java
@@ -0,0 +1,22 @@
+package org.project.global.config.security;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@RequiredArgsConstructor
+public class CookieConfig {
+
+    @Value("${app.cookie.domain:}")
+    private String domain;
+
+    @Value("${app.cookie.secure}")
+    private boolean secure;
+
+    @Value("${app.cookie.same-site}")
+    private String sameSite;
+
+}
