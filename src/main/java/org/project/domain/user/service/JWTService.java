5901eb2f5860c45a0fa476b1c2cfd91104a16ae5
koobontak <kbt82883@gmail.com>
2026-01-10 23:31:00 +0900

    [feat] 테스트용 엑세스 토큰 발급 API

---
+++ src/main/java/org/project/domain/user/service/JWTService.java
@@ -0,0 +1,20 @@
+package org.project.domain.user.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.project.global.util.JWTUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JWTService {
+
+    private final JWTUtil jwtUtil;
+
+    // 토큰 발급기를 위한 메서드입니다
+    public void createToken(HttpServletResponse response) {
+
+        String access = jwtUtil.generateAccessToken(1L);
+        response.setHeader("access-token", access);
+    }
+}
