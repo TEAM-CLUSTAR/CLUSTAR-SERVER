6aed3ad61831d409d4091139a16de25b1fe70e30
koobontak <kbt82883@gmail.com>
2026-01-14 00:50:07 +0900

    [feat] 이미지/파일 조회 쿼리

--- src/main/java/org/project/domain/memo/repository/MemoFileRepository.java
+++ src/main/java/org/project/domain/memo/repository/MemoFileRepository.java
@@ -3,5 +3,9 @@ package org.project.domain.memo.repository;
 import org.project.domain.memo.entity.MemoFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoFileRepository extends JpaRepository<MemoFile, Long> {
+
+    List<MemoFile> findByMemoIdIn(List<Long> memoIds);
 }
--- src/main/java/org/project/domain/memo/repository/MemoImageRepository.java
+++ src/main/java/org/project/domain/memo/repository/MemoImageRepository.java
@@ -3,5 +3,9 @@ package org.project.domain.memo.repository;
 import org.project.domain.memo.entity.MemoImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoImageRepository extends JpaRepository<MemoImage, Long> {
+
+    List<MemoImage> findByMemoIdIn(List<Long> memoIds);
 }
