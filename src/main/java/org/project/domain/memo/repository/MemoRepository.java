8080033f92bbff201974296c407b3bffb9b8cb5d
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-01-13 17:19:05 +0900

    chore: import수정

--- src/main/java/org/project/domain/memo/repository/MemoRepository.java
+++ src/main/java/org/project/domain/memo/repository/MemoRepository.java
@@ -1,13 +1,10 @@
 package org.project.domain.memo.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.project.domain.memo.entity.Memo;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface MemoRepository extends JpaRepository<Memo,Long>, MemoRepositoryCustom {
