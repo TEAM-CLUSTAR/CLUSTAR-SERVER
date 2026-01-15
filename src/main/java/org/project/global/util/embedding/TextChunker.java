a2e16759bd41a1359dd0a3df621b8624b3df090e
koobontak <kbt82883@gmail.com>
2026-01-16 05:39:12 +0900

    [feat] 청크 유틸 클래스 작성

---
+++ src/main/java/org/project/global/util/embedding/TextChunker.java
@@ -0,0 +1,25 @@
+package org.project.global.util.embedding;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class TextChunker {
+
+    private static final int CHUNK_SIZE = 500;
+
+    public List<String> chunk(String text) {
+
+        List<String> chunks = new ArrayList<>();
+
+        for (int i = 0; i < text.length(); i += CHUNK_SIZE) {
+            int end = Math.min(text.length(), i + CHUNK_SIZE);
+            chunks.add(text.substring(i, end));
+        }
+
+        return chunks;
+    }
+}
+
