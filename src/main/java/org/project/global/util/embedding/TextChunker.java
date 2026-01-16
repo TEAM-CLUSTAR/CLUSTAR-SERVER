d1966ef57fba7c4959a44014de39dcf51bc0244d
koobontak <kbt82883@gmail.com>
2026-01-16 22:19:09 +0900

    [feat] 청크 유틸 클래스 npe 예외처리

--- src/main/java/org/project/global/util/embedding/TextChunker.java
+++ src/main/java/org/project/global/util/embedding/TextChunker.java
@@ -12,6 +12,10 @@ public class TextChunker {
 
     public List<String> chunk(String text) {
 
+        if (text == null || text.isEmpty()) {
+            return new ArrayList<>();
+        }
+
         List<String> chunks = new ArrayList<>();
 
         for (int i = 0; i < text.length(); i += CHUNK_SIZE) {
