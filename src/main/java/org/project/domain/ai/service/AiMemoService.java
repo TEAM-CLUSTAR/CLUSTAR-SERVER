e49f3fbb2da2fb0968296f7411987435da0b4933
koobontak <kbt82883@gmail.com>
2026-01-18 20:03:36 +0900

    [del] 이전 버전 AI API 삭제

--- src/main/java/org/project/domain/ai/service/AiMemoService.java
+++
@@ -1,9 +0,0 @@
-package org.project.domain.ai.service;
-
-import org.project.domain.ai.dto.request.AiMemoCreateRequest;
-import org.project.domain.ai.dto.response.AiMemoCreateResponse;
-
-public interface AiMemoService {
-
-    AiMemoCreateResponse createAiMemo(Long userId, AiMemoCreateRequest request);
-}
