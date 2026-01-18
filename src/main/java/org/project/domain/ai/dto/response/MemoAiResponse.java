625fc68d7b1f609898474652cc3ff943bc04882d
JO HYODONG <160843817+hyodongg@users.noreply.github.com>
2026-01-19 04:40:54 +0900

    chore: 메모 ai응답에 title포함

--- src/main/java/org/project/domain/ai/dto/response/MemoAiResponse.java
+++ src/main/java/org/project/domain/ai/dto/response/MemoAiResponse.java
@@ -6,17 +6,19 @@ import org.project.domain.ai.dto.MemoAiOptions;
 import java.util.List;
 
 public record MemoAiResponse(
+        String title,
         String content,
         MemoAiOptions option,
         List<Long> memoIds,
         String usedPrompt
 ) {
     public static MemoAiResponse of(
+            String title,
             String content,
             MemoAiOptions option,
             List<Long> memoIds,
             String usedPrompt
     ) {
-        return new MemoAiResponse(content, option, memoIds, usedPrompt);
+        return new MemoAiResponse(title, content, option, memoIds, usedPrompt);
     }
 }
