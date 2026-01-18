d4516f14c13d195cdbe45ab2717908142086ec7c
koobontak <kbt82883@gmail.com>
2026-01-18 17:28:10 +0900

    [feat] 이전 transformer 클래스 삭제

--- src/main/java/org/project/global/util/memoMedia/PdfTextExtractor.java
+++ src/main/java/org/project/global/util/memoMedia/PdfTextExtractor.java
@@ -1,18 +1,18 @@
-package org.project.global.util.memoMedia;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-import org.springframework.stereotype.Component;
-
-@Component
-public class PdfTextExtractor {
-
-    public String extract(byte[] pdfBytes) {
-        try (PDDocument document = PDDocument.load(pdfBytes)) {
-            PDFTextStripper stripper = new PDFTextStripper();
-            return stripper.getText(document);
-        } catch (Exception e) {
-            throw new RuntimeException("PDF 텍스트 추출 실패", e);
-        }
-    }
-}
+//package org.project.global.util.memoMedia;
+//
+//import org.apache.pdfbox.pdmodel.PDDocument;
+//import org.apache.pdfbox.text.PDFTextStripper;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class PdfTextExtractor {
+//
+//    public String extract(byte[] pdfBytes) {
+//        try (PDDocument document = PDDocument.load(pdfBytes)) {
+//            PDFTextStripper stripper = new PDFTextStripper();
+//            return stripper.getText(document);
+//        } catch (Exception e) {
+//            throw new RuntimeException("PDF 텍스트 추출 실패", e);
+//        }
+//    }
+//}
