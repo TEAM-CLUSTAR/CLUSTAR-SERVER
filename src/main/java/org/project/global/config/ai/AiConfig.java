1f56b8383315eb1896e0f6b487e5c5c037dea2e8
koobontak <kbt82883@gmail.com>
2026-01-20 11:00:04 +0900

    [feat] chat memory Bean 등록

--- src/main/java/org/project/global/config/ai/AiConfig.java
+++ src/main/java/org/project/global/config/ai/AiConfig.java
@@ -1,6 +1,10 @@
 package org.project.global.config.ai;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +12,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AiConfig {
 
+//    @Bean
+//    public ChatClient chatClient(ChatModel chatModel) {
+//        return ChatClient.builder(chatModel)
+//                .build();
+//    }
+
     @Bean
-    public ChatClient chatClient(ChatModel chatModel) {
+    public ChatClient chatClient(ChatModel chatModel, ChatMemory chatMemory) {
         return ChatClient.builder(chatModel)
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
+                .build();
+    }
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(repository)
+                .maxMessages(20)
                 .build();
     }
 }
