4629354d058b6a2cabd1451ab8d13a8701fe4c66
koobontak <kbt82883@gmail.com>
2026-01-20 17:37:17 +0900

    [test] 테스트 코드 수정

--- src/test/java/org/project/domain/ai/rag/RagPipelineRelevancyTest.java
+++ src/test/java/org/project/domain/ai/rag/RagPipelineRelevancyTest.java
@@ -2,6 +2,9 @@ package org.project.domain.ai.rag;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.project.domain.ai.dto.MemoAiOptions;
 import org.project.domain.ai.dto.request.MemoAiRequest;
 import org.project.domain.ai.dto.response.MemoAiResponse;
@@ -13,6 +16,7 @@ import org.project.domain.ai.rag.F.augment.dto.RagPrompt;
 import org.project.domain.ai.rag.G.generate.RagGenerator;
 import org.project.domain.ai.rag.pipeline.DefaultRagPipeline;
 import org.project.global.config.chatModel.TestChatModelConfig;
+import org.project.global.config.chatModel.YesChatModel;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.evaluation.RelevancyEvaluator;
 import org.springframework.ai.document.Document;
@@ -21,9 +25,7 @@ import org.springframework.ai.evaluation.EvaluationResponse;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
@@ -32,28 +34,28 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.given;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {
-        DefaultRagPipeline.class,
-        TestChatModelConfig.class
-})
+@ExtendWith(MockitoExtension.class)
 class RagPipelineRelevancyTest {
 
-    @Autowired
+    @InjectMocks
     DefaultRagPipeline ragPipeline;
 
-    @Autowired
-    ChatClient.Builder chatClientBuilder;
+    ChatClient.Builder chatClientBuilder =
+            ChatClient.builder(new YesChatModel());
 
-    @MockBean
+    @Mock
     RagQueryHandler ragQueryHandler;
-    @MockBean
+
+    @Mock
     RagRetriever ragRetriever;
-    @MockBean
+
+    @Mock
     RagAugmenter ragAugmenter;
-    @MockBean
+
+    @Mock
     RagGenerator ragGenerator;
-    @MockBean
+
+    @Mock
     VectorStore vectorStore;
 
     @Test
@@ -66,25 +68,6 @@ class RagPipelineRelevancyTest {
                 List.of(1L)
         );
 
-        given(ragGenerator.generate(any()))
-                .willReturn("이 문서는 메모의 핵심을 요약한 내용입니다.");
-
-        given(vectorStore.similaritySearch(any(SearchRequest.class)))
-                .willReturn(
-                        List.of(new Document("이 문서는 메모의 핵심을 요약한 내용입니다."))
-                );
-
-        given(ragAugmenter.augment(
-                any(RagQuery.class),
-                anyList()
-        )).willReturn(
-                RagPrompt.of(
-                        "SYSTEM",
-                        "이 문서는 메모의 핵심을 요약한 내용입니다.",
-                        "이 메모 뭐야?"
-                )
-        );
-
         given(ragQueryHandler.handle(any(Long.class), any(MemoAiRequest.class)))
                 .willReturn(
                         RagQuery.of(
@@ -95,9 +78,20 @@ class RagPipelineRelevancyTest {
                         )
                 );
 
+        given(ragAugmenter.augment(any(RagQuery.class), anyList()))
+                .willReturn(
+                        RagPrompt.of(
+                                "SYSTEM",
+                                "이 문서는 메모의 핵심을 요약한 내용입니다.",
+                                request.userPrompt()
+                        )
+                );
+
+        given(ragGenerator.generate(any()))
+                .willReturn("이 문서는 메모의 핵심을 요약한 내용입니다.");
 
         // when
-        MemoAiResponse response = ragPipeline.run(1L, request);
+        MemoAiResponse response = ragPipeline.run(1L, 1L, request);
 
         // then
         EvaluationRequest evaluationRequest =
@@ -115,5 +109,4 @@ class RagPipelineRelevancyTest {
 
         assertThat(evaluationResponse.isPass()).isTrue();
     }
-
 }
