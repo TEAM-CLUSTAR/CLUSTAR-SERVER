a56f4f675176b3404c04af96ffafd3e743bafbd0
bontak <142137769+kbt82883@users.noreply.github.com>
2026-01-21 00:54:35 +0900

    [fix] 챗메모리 advisor 클래스 수정 (#82)

--- src/main/java/org/project/global/config/ai/AiConfig.java
+++ src/main/java/org/project/global/config/ai/AiConfig.java
@@ -1,7 +1,7 @@
 package org.project.global.config.ai;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
@@ -16,7 +16,7 @@ public class AiConfig {
     public ChatClient chatClient(ChatModel chatModel, ChatMemory chatMemory) {
         return ChatClient.builder(chatModel)
                 .defaultAdvisors(
-                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                        PromptChatMemoryAdvisor.builder(chatMemory).build()
                 )
                 .build();
     }
